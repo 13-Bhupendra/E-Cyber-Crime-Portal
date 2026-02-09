9bca41469e4ec3330eab24c60caab5b1091b3e58
13-Bhupendra <bhupendrakp2008@gmail.com>
2026-02-09 17:06:04 +0530

    09/02/2026

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(
   cors({
     origin: ["http://localhost:5173",
-      "https://e-cyber-crime-portal.vercel.app/"
+      "https://e-cyber-crime-portal.vercel.app"
     ],
     credentials: true,
   })
