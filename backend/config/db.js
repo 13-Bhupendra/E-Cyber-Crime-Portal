33fb93ee4a5e6020cd07c7a7961029abc500477c
HK-Bhupendra <bhupendrahkdigiverse@gmail.com>
2026-02-22 16:03:20 +0530

    22/02/2026

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,37 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config()
 
-export const connectDB = async ()=>{
-    try {
-       await mongoose.connect(process.env.MONGODB_ATLAS_URL)
-       console.log("MongoDB connection successfull !")
-    } catch (error) {
-        console.log("mongoDB server connection failed !")
+export const connectDB = async () => {
+  try {
+    if (!process.env.MONGODB_ATLAS_URL) {
+      throw new Error("MONGODB_ATLAS_URL is not defined");
     }
-}
+
+    await mongoose.connect(process.env.MONGODB_ATLAS_URL, {
+      dbName: "E-cyber portal",
+    });
+
+    console.log("MongoDB connected successfully 🚀");
+  } catch (error) {
+    console.error("MongoDB connection failed ❌");
+    console.error(error.message);
+    process.exit(1); 
+  }
+};
+
+
+
+
+// import mongoose from "mongoose";
+// import dotenv from "dotenv"
+// dotenv.config()
+
+// export const connectDB = async ()=>{
+//     try {
+//        await mongoose.connect(process.env.MONGODB_ATLAS_URL)
+//        console.log("MongoDB connection successfull !")
+//     } catch (error) {
+//         console.log("mongoDB server connection failed !")
+//     }
+// }
+
+
